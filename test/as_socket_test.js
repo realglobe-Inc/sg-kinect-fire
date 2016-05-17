4a52b4eeab41c7cc6365fc7779f3ab8294296eef
Taka Okunishi <okunishitaka.com@gmail.com>
2016-05-17 12:16:56 +0900

    Add socket

---
+++ test/as_socket_test.js
@@ -0,0 +1,25 @@
+/**
+ * Test case for asSocket.
+ * Runs with mocha.
+ */
+'use strict'
+
+const asSocket = require('../lib/as_socket.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('as-socket', () => {
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('As socket', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
---
+++ test/combine_reducer_test.js
@@ -0,0 +1,25 @@
+/**
+ * Test case for combineReducer.
+ * Runs with mocha.
+ */
+'use strict'
+
+const combineReducer = require('../lib/reducing/combine_reducer.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('combine-reducer', () => {
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Combine reducer', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
